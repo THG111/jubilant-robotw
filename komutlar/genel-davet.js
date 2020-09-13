c8049aa84f2e3e9a5f480a2fc7d665f2f88d5e5c
Glitch (rounded-slow-jaxartosaurus) <none>
2020-09-13 20:20:14 +0000

    📺🖐 Checkpoint ./komutlar/yardım-yardım.js:10916596/252 ./komutlar/genel-davet.js:10916596/411

--- komutlar/genel-davet.js
+++ komutlar/genel-davet.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
         
         .setTitle(`${client.user.username} Davet Menüsü `)
-        .setDescription(`<a:hypesquad1:750076071721828452> **Botun Davet Linki İçin** [TIKLA](https://discord.com/oauth2/authorize?client_id=713713727794446397&scope=bot&permissions=8) \n <a:hypesquad1:750076071721828452> **Destek Sunucusu İçin** [TIKLA](https://discord.gg/H54BNFR) \n <a:hypesquad1:750076071721828452> **Web Sitemizi Ziyaret etmek İçin** [TIKLA](https://piratebots.glitch.me)`)
+        .setDescription(` **Botun Davet Linki İçin** [TIKLA](https://discord.com/oauth2/authorize?client_id=754793081043288165&scope=bot&permissions=8) \n   \n `)
         .setThumbnail(client.user.avatarURL)
         .setFooter(`${message.author.username} Başarıyla ${ayarlar.prefix} Davet Sistemi Kullandı`, message.author.avatarURL)
     .setColor(`#ffd100`)
